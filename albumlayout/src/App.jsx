84977f5adb934c77f336e851903f7cfccce1313e
Müzəffər Əliyev <muzaffar.aliyev1121@uni.edu.az>
2021-04-07 17:44:31 +0400

    Finished.

--- albumlayout/src/App.jsx
+++ albumlayout/src/App.jsx
@@ -8,23 +8,29 @@ import {
   CardMedia,
   CssBaseline,
   Grid,
+  Button,
   Toolbar,
   Container,
 } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
 
+import useStyles from './styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const App = () => {
+  const classes = useStyles();
   return (
     <>
       <CssBaseline />
       <AppBar position='relative'>
         <Toolbar>
-          <PhotoCamera />
+          <PhotoCamera className={classes.icon} />
           <Typography variant='h6'>Photo Album</Typography>
         </Toolbar>
       </AppBar>
       <main>
-        <div>
+        <div className={classes.container}>
           <Container maxWidth='sm'>
             <Typography
               variant='h2'
@@ -42,9 +48,67 @@ const App = () => {
             >
               Hi guys, this is a cool photo album
             </Typography>
+            <div className={classes.button}>
+              <Grid container spacing={2} justify='center'>
+                <Grid item>
+                  <Button variant='contained' color='primary'>
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant='outlined' color='primary'>
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
           </Container>
         </div>
+        <Container className={classes.cardGrid} maxWidth='md'>
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image='https://source.unsplash.com/random'
+                    title='Image title'
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant='h5'>
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is media card.Create description for content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size='small' color='primary'>
+                      View
+                    </Button>
+                    <Button size='small' color='secondary'>
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
+      <footer className={classes.footer}>
+        <Typography variant='h6' align='center' gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant='subtitle1'
+          align='center'
+          color='textSecondary'
+          gutterBottom
+        >
+          Something for footer purposes.
+        </Typography>
+      </footer>
     </>
   );
 };
