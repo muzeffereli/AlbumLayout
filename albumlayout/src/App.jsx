64d49cad0f20ef0101b61db4331b351d2eed9c0c
Müzəffər Əliyev <muzaffar.aliyev1121@uni.edu.az>
2021-04-07 04:11:25 +0400

    First Commit

---
+++ albumlayout/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+} from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position='relative'>
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant='h6'>Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth='sm'>
+            <Typography
+              variant='h2'
+              align='center'
+              color='textPrimary'
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant='h5'
+              align='center'
+              color='textSecondary'
+              paragraph
+            >
+              Hi guys, this is a cool photo album
+            </Typography>
+          </Container>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;
